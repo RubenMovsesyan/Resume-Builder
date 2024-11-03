e2e0d4c55c96a21116a09115bb9887c9b0d4971e
Ruben Movsesyan <movsesyan.ruben@gmail.com>
2024-11-02 17:41:25 -0700

    figured out the text wrapping, now need to figure out line spacing

--- src/pdf.rs
+++ src/pdf.rs
@@ -1,10 +1,12 @@
 use crate::resume::*;
+use glyph_brush_layout::ab_glyph::Font;
+use glyph_brush_layout::GlyphPositioner;
+use itertools::Itertools;
 use printpdf::*;
-use latex::{DocumentClass, Element, Document, Section, Align};
-use genpdf::*;
 use std::fs::File;
 use std::io::BufWriter;
 
+use std::io::Read;
 use std::io::Write;
 
 enum FontStyle {
@@ -70,37 +72,13 @@ fn capitalize(string: String) -> String {
     output
 }
 
-// pub fn gen_latex_from_resume(resume: &Resume) {
-//     let mut doc = Document::new(DocumentClass::Article);
+fn mm_to_px(mm: f32) -> f32 {
+    mm * 96.0 / 25.4
+}
 
-//     doc.preamble.title("Test Document");
-//     doc.preamble.author("Me");
-
-//     doc.push(Element::TitlePage)
-//     .push(Element::ClearPage)
-//     .push(Element::TableOfContents)
-//     .push(Element::ClearPage);
-
-//     let mut section_1 = Section::new("Section 1");
-//     section_1.push("This is the first Section")
-//     .push("And the second part of the first section");
-
-//     doc.push(section_1);
-
-//     let rendered = latex::print(&doc).unwrap();
-
-//     // println!("{}", rendered);
-
-//     let mut f = File::create("./res/test.tex").unwrap();
-//     let _ = write!(f, "{}", rendered);
-// }
-
-// pub fn generate_pdf_from_resume(resume: &Resume) {
-//     let font_family = genpdf::fonts::from_files("./fonts", "times", None).expect("Failed to load font");
-    
-
-//     let mut doc = genpdf::Document::new(font_family);
-// }
+fn px_to_mm(px: f32) -> f32 {
+    px * 25.4 / 96.0
+}
 
 pub fn create_pdf_from_resume(resume: &Resume) {
     // Create a pdf letter size
@@ -109,43 +87,137 @@ pub fn create_pdf_from_resume(resume: &Resume) {
     let current_layer = doc.get_page(page1).get_layer(layer1);
 
     // Fonts
-    let font = doc.add_builtin_font(BuiltinFont::TimesRoman).unwrap();
-    let font_bold = doc.add_builtin_font(BuiltinFont::TimesBold).unwrap();
+    // Load in the font data
+    let normal_font = {
+        let mut font_file = File::open("./fonts/times_new_roman.ttf").unwrap();
+        let mut font_data = Vec::with_capacity(font_file.metadata().unwrap().len() as usize);
+        font_file.read_to_end(&mut font_data).unwrap();
+        font_data
+    };
 
-    // FIXME fix repeat code
+    let bold_font = {
+        let mut font_file = File::open("./fonts/times_new_roman_bold.ttf").unwrap();
+        let mut font_data = Vec::with_capacity(font_file.metadata().unwrap().len() as usize);
+        font_file.read_to_end(&mut font_data).unwrap();
+        font_data
+    };
 
-    current_layer.begin_text_section();
+    // Load the font reference for glyph brush layout
+    let gbl_normal_font = glyph_brush_layout::ab_glyph::FontRef::try_from_slice(&normal_font).unwrap();
+    let gbl_bold_font = glyph_brush_layout::ab_glyph::FontRef::try_from_slice(&bold_font).unwrap();
+    let gbl_fonts = &[gbl_normal_font, gbl_bold_font];
 
-    current_layer.set_text_cursor(Mm(10.0), Mm(269.4));
+    let normal_writing_font = doc.add_external_font(normal_font.as_slice()).unwrap();
+    let bold_writing_font = doc.add_external_font(bold_font.as_slice()).unwrap();
+
+    let mut current_line_height = 269.4;
+
+    let mut write_to_page = |text_to_write: &str, left_margin: f32, width: f32, font_size: f32, font_id: usize| {
+        // calculate the glyph positions using glyph_brush_layout
+        let glyphs = glyph_brush_layout::Layout::default().calculate_glyphs(
+            gbl_fonts,
+            &glyph_brush_layout::SectionGeometry {
+                bounds: (mm_to_px(width), f32::INFINITY),
+                ..Default::default()
+            },
+            &[glyph_brush_layout::SectionText {
+                text: text_to_write,
+                scale: gbl_fonts[font_id].pt_to_px_scale(font_size).unwrap(),
+                font_id: glyph_brush_layout::FontId(font_id),
+            }],
+        );
+
+        // make sure the number of glyphs matches the number of chars in the sample text
+        assert_eq!(glyphs.len(), text_to_write.chars().count());
+
+        // group the glyphs by y position
+        let line_starts = glyphs
+            .iter()
+            .enumerate()
+            .chunk_by(|(_, glyph)| glyph.glyph.position.y)
+            .into_iter()
+            .map(|(y, mut group)| (y, group.next().unwrap().0))
+            .collect::<Vec<_>>();
+
+        // get the minimum y position
+        let min = glyphs
+            .iter()
+            .map(|glyph| glyph.glyph.position.y)
+            .fold(f32::INFINITY, |a, b| a.min(b));
+
+        // need a peekable iterator so we can see where the next line starts
+        let mut iter = line_starts.iter().peekable();
+
+        let mut line_diff = 0.0;
+
+        // iterate over the line_starts and draw the text
+        loop {
+            // get the next line start, if there is none then we break out of the loop
+            let Some((y, start)) = iter.next() else {
+                break;
+            };
+
+            // peek into the line start after that to get the end index,
+            // if there is none (we're at the last line of the loop) then we use the length of the sample text
+            let end = if let Some((_, end)) = iter.peek() {
+                *end
+            } else {
+                text_to_write.chars().count()
+            };
+
+            // Slice up the text
+            // if you know you're only dealing with ASCII characters you can simplify this as
+            // `let line = &sample[*start..end];`
+            // which saves on an allocation to a String;
+            // or you can use char_indices to get the byte indices and slice that way
+            let line = text_to_write
+                .chars()
+                .skip(*start)
+                .take(end - start)
+                .collect::<String>();
+
+            let font = match font_id {
+                1 => {
+                    &bold_writing_font
+                } 
+                _ => {
+                    &normal_writing_font
+                }            
+            };
+
+            current_layer.use_text(
+                line.trim(),
+                font_size,
+                Mm(left_margin),
+                Mm(current_line_height + px_to_mm(min) - px_to_mm(*y)),
+                font,
+            );
+            // This needs to be changed because it is very inefficient
+            line_diff = px_to_mm(*y);
+        }
+        current_line_height -= line_diff;
+    };
 
     // Add Skills
-    current_layer.set_font(&font, FontStyle::SectionHeader.get_font_size()); // Section Header
-    current_layer.set_fill_color(FontStyle::SectionHeader.get_font_color());
-    current_layer.write_text("Skills", &font);
-    current_layer.set_line_height(25.0);
-    current_layer.add_line_break();
-    for category in resume.get_skills().iter() {
-        current_layer.set_font(&font_bold, FontStyle::ItemHeader.get_font_size());
-        current_layer.set_fill_color(FontStyle::ItemHeader.get_font_color());
-        current_layer.write_text(capitalize(category.0.clone()), &font_bold);
-        current_layer.set_line_height(14.0);
-        current_layer.add_line_break();
+    write_to_page("Skills", 20.0, 160.0, 25.0, 1);
 
-        current_layer.set_font(&font, FontStyle::Normal.get_font_size());
-        current_layer.set_fill_color(FontStyle::Normal.get_font_color());
+    for category in resume.get_skills().iter() {
+        write_to_page(&capitalize(category.0.clone()), 20.0, 160.0, 25.0, 1);
+
         let mut skills = String::new();
         for item in category.1.iter() {
             skills.push_str(&std::format!("{} ", item.sortable));
         }
-        current_layer.write_text(skills.clone(), &font);
-        current_layer.set_line_height(25.0);
-        current_layer.add_line_break();
+        write_to_page(&skills, 20.0, 160.0, 14.0, 0);
     }
 
+
+    /*
     // Add Work Experience
-    current_layer.set_font(&font, FontStyle::SectionHeader.get_font_size()); // Section Header
-    current_layer.set_fill_color(FontStyle::SectionHeader.get_font_color());
-    current_layer.write_text("Work Experience", &font);
+    // current_layer.set_font(&font, FontStyle::SectionHeader.get_font_size()); // Section Header
+    // current_layer.set_fill_color(FontStyle::SectionHeader.get_font_color());
+    // current_layer.write_text("Work Experience", &font);
+    write_section_header(&current_layer, "Work Experience");
 
     for experience in resume.get_work_experience().iter() {
         let e = &experience.sortable;
@@ -203,9 +275,10 @@ pub fn create_pdf_from_resume(resume: &Resume) {
     // Add Education
     current_layer.set_line_height(FontStyle::SectionHeader.get_font_size());
     current_layer.add_line_break();
-    current_layer.set_font(&font, FontStyle::SectionHeader.get_font_size()); // Section Header
-    current_layer.set_fill_color(FontStyle::SectionHeader.get_font_color());
-    current_layer.write_text("Education", &font);
+    // current_layer.set_font(&font, FontStyle::SectionHeader.get_font_size()); // Section Header
+    // current_layer.set_fill_color(FontStyle::SectionHeader.get_font_color());
+    // current_layer.write_text("Education", &font);
+    write_section_header(&current_layer, "Education");
     for education in resume.get_education() {
         let e = &education.sortable;
 
@@ -323,8 +396,9 @@ pub fn create_pdf_from_resume(resume: &Resume) {
         current_layer.set_fill_color(FontStyle::Normal.get_font_color());
         current_layer.write_text(format!("* {} GPA", e.get_gpa()), &font);
     }
+    */
 
-    current_layer.end_text_section();
+    // current_layer.end_text_section();
 
     // Save the pdf in the designated directory
     doc.save(&mut BufWriter::new(File::create("./res/test.pdf").unwrap()))
